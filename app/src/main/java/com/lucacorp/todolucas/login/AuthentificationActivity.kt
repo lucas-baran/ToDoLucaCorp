07e2013b106e9e7e479145a0147747f3c3d249ac
Lucas Baran <lucas.baran7@gmail.com>
2022-01-03 16:37:05 +0100

    changement place navcontroller

--- app/src/main/java/com/lucacorp/todolucas/login/AuthentificationActivity.kt
+++ app/src/main/java/com/lucacorp/todolucas/login/AuthentificationActivity.kt
@@ -11,20 +11,8 @@ import com.lucacorp.todolucas.databinding.FragmentAuthentificationBinding
 
 class AuthentificationActivity : AppCompatActivity() {
 
-    private lateinit var navController: NavController
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentification)
-
-        navController = findNavController(R.id.nav_host_fragment)
-
-        findViewById<Button>(R.id.logInAuthButton).setOnClickListener {
-            navController.navigate(R.id.action_authentificationFragment_to_loginFragment)
-        }
-
-        findViewById<Button>(R.id.signUpAuthButton).setOnClickListener {
-            navController.navigate(R.id.action_authentificationFragment_to_signupFragment)
-        }
     }
 }
--- app/src/main/java/com/lucacorp/todolucas/login/AuthentificationFragment.kt
+++ app/src/main/java/com/lucacorp/todolucas/login/AuthentificationFragment.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.lucacorp.todolucas.R
 import com.lucacorp.todolucas.databinding.FragmentAuthentificationBinding
 
 class AuthentificationFragment: Fragment() {
 
     private lateinit var binding: FragmentAuthentificationBinding
+
+    private lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,5 +24,19 @@ class AuthentificationFragment: Fragment() {
     ): View? {
         binding = FragmentAuthentificationBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = findNavController()
+
+        binding.logInAuthButton.setOnClickListener {
+            navController.navigate(R.id.action_authentificationFragment_to_loginFragment)
+        }
+
+        binding.signUpAuthButton.setOnClickListener {
+            navController.navigate(R.id.action_authentificationFragment_to_signupFragment)
+        }
     }
 }
