6f8d5b8a4fac8a208a57bb5a60aa1f9928e29ca3
Lucas Baran <lucas.baran7@gmail.com>
2022-01-03 17:23:22 +0100

    fix login

--- app/src/main/java/com/lucacorp/todolucas/login/LoginForm.kt
+++ app/src/main/java/com/lucacorp/todolucas/login/LoginForm.kt
@@ -1,6 +1,12 @@
 package com.lucacorp.todolucas.login
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class LoginForm(
+    @SerialName("email")
     val email: String,
+    @SerialName("password")
     val password: String,
 )
