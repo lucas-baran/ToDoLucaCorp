08a605b49f190b5d1c84db6f20643ed2f430bd74
Lucas Baran <lucas.baran7@gmail.com>
2022-01-03 17:24:45 +0100

    fix login

--- app/src/main/java/com/lucacorp/todolucas/login/LoginResponse.kt
+++ app/src/main/java/com/lucacorp/todolucas/login/LoginResponse.kt
@@ -1,5 +1,10 @@
 package com.lucacorp.todolucas.login
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class LoginResponse(
+    @SerialName("token")
     val token : String
 )
